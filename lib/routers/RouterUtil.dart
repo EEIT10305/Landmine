d1dd799ede52c5dc94627892e626bc180a08d89d
Kat <ivory0504@gmail.com>
2021-01-27 15:25:12 +0800

    遠端載db

--- lib/routers/RouterUtil.dart
+++ lib/routers/RouterUtil.dart
@@ -9,9 +9,9 @@ import 'package:landmine/widgets/pages/jp_show_kana/JPAddKanaPage.dart';
 class RouterUtil{
   static Map<String, WidgetBuilder> router = <String, WidgetBuilder> {
     "landmine": (context) => LandminePage(),
-    "mytree": (context) => MyTree(),
-    "homescreen":(context) => HomeScreen(),
-    "fileprovider":(context) => FileProviderPage(),
+    "homescreen":(context) => HomeScreen(),//資料丟給下一頁再傳回來
+    "mytree": (context) => MyTree(),//inheritedWidget練習
+    "fileprovider":(context) => FileProviderPage(),//寫檔讀檔練習
     "dbexample":(context) => DbExamplePage(),
     "jpdata":(context) => JPAddKanaPage(),
   };
